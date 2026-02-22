8454e964b783954e2380764a30b71a5ac075a22a
SamiUllahDev <samiullahkhial@gmail.com>
2026-02-22 14:42:07 +0500

    dfjkslkdjfmslkdjkfmsdoklz

--- calculator_drive/settings.py
+++ calculator_drive/settings.py
@@ -179,12 +179,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # For production: python manage.py collectstatic
 
-# Media files (User uploads)
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'  # Directory for user-uploaded files (blog images, avatars, etc.)
+STATIC_URL = 'static/'
+STATIC_ROOT = "/var/www/calculator_drive/static/"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = "/var/www/calculator_drive/media/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
